96dcb3a487b0232adca6335a83ca3aa072b41463
Ge5pi <157716905+Ge5pi@users.noreply.github.com>
2025-03-18 22:30:17 +0500

    images+product

--- app/src/main/java/com/example/foodresq/views/AddPosition.kt
+++ app/src/main/java/com/example/foodresq/views/AddPosition.kt
@@ -2,15 +2,20 @@ package com.example.foodresq.views
 
 import android.app.Activity
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
+import androidx.activity.ComponentActivity
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.bumptech.glide.Glide
 import com.example.foodresq.R
 import com.example.foodresq.adaptersEtc.SessionManager
 import com.example.foodresq.classes.DbHelper
@@ -19,8 +24,9 @@ import com.example.foodresq.classes.Restaurant
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
+import com.google.firebase.storage.ktx.storage
 
-class AddPosition : Activity() {
+class AddPosition : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_position)
@@ -46,6 +52,16 @@ class AddPosition : Activity() {
         val positionCollection = fireDb.collection("positions")
         var currentRestFactId = ""
         var currentRestId = 0
+
+        val imagePos = findViewById<ImageView>(R.id.imagePos)
+        val initialImageUri = "https://firebasestorage.googleapis.com/v0/b/foodresq-bc5d2.appspot.com/o/empty_avatar.png?alt=media&token=246e42a8-8e6b-4fac-9267-2f85568860e9"
+        Glide.with(this)
+            .load(initialImageUri)
+            .into(imagePos)
+
+        imagePos.setOnClickListener {
+
+        }
 
         button.setOnClickListener {
             val factName = name.text.trim()
@@ -88,11 +104,11 @@ class AddPosition : Activity() {
                                         }
                                     }
                                 }
-                }.addOnSuccessListener {
-                    val intent = Intent(this, AddImage::class.java)
-                    intent.putExtra("name", factName.toString())
-                    intent.putExtra("desc", factDesc.toString())
-                    intent.putExtra("price", factPrice)
+                }.addOnSuccessListener { openGallery() }.addOnSuccessListener {
+                    val intent = Intent(this, Home::class.java)
+                    //intent.putExtra("name", factName.toString())
+                    //intent.putExtra("desc", factDesc.toString())
+                    //intent.putExtra("price", factPrice)
                     startActivity(intent)
                 }}
         }
@@ -100,5 +116,49 @@ class AddPosition : Activity() {
 
 
 
-    
+    private val galleryLauncher = registerForActivityResult(ActivityResultContracts.GetContent()) { uri ->
+        if (uri != null) {
+            uploadImageToFirestore(uri)
+        }
+    }
+
+    private fun openGallery() {
+        galleryLauncher.launch("image/*")
+    }
+
+    private fun uploadImageToFirestore(uri: Uri) {
+        val avatar = findViewById<ImageView>(R.id.imagePos)
+        val storage = Firebase.storage
+        val storageRef = uri.lastPathSegment?.let { storage.reference.child(it) }
+        val uploadTask = storageRef?.putFile(uri)
+        uploadTask?.addOnSuccessListener {
+            storageRef.downloadUrl.addOnSuccessListener { downloadUri ->
+                Glide.with(this)
+                    .load(downloadUri)
+                    .into(avatar)
+                saveImageToFirestore(downloadUri.toString())
+            }
+        }
+    }
+
+    private fun saveImageToFirestore(downloadUri: String) {
+        val bundle = intent.extras
+        val name = bundle?.getString("name")
+        val desc = bundle?.getString("desc")
+        val price = bundle?.getInt("price")
+        val fireDb = Firebase.firestore
+        val auth = Firebase.auth
+        val current = auth.currentUser
+        fireDb.collection("positions").whereEqualTo("name", name).whereEqualTo("desc", desc)
+            .whereEqualTo("price", price).get().addOnSuccessListener { positions ->
+                if (positions.isEmpty) {
+                    Log.i("AddPosition", "PROFILE EDIT: CANNOT FIND USER")
+                } else {
+                    for (pos in positions) {
+                        val userRef = fireDb.collection("positions").document(pos.id)
+                        userRef.update("ava", downloadUri)
+                    }
+                }
+            }
+    }
 }
